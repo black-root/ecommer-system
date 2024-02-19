c4ce8ff9af9d3f987aa1acbc10836469a2aa8704
Ronald Ibanez <ronald.ibanez1994@gmail.com>
2024-02-18 21:50:08 -0600

    demos Apis are created. Works fine

--- payment-api/src/main/java/com/blackroot/PaymentApi.java
+++ payment-api/src/main/java/com/blackroot/PaymentApi.java
@@ -1,13 +1,12 @@
-package com.blackroot.ecommerceapi;
+package com.blackroot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcommerceApiApplication {
+public class PaymentApi {
 
     public static void main(String[] args) {
-        SpringApplication.run(EcommerceApiApplication.class, args);
+        SpringApplication.run(PaymentApi.class, args);
     }
-
 }
